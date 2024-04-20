8a008733935247360f9e2bfc68ab1582d7111b7b
Anth0s <0ididmn0@gmail.com>
2024-04-20 05:35:13 +0300

    added test folder

---
+++ src/Test/GameTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import BaumansGate.Game.Game;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void testPlayerWin() {
+        Game game = new Game();
+        
+    }
+}
